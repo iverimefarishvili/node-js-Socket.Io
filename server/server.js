3e0125108dae5642a29b743b3679002519799e5c
iverimefarishvili <mefarishvili922@gmail.com>
2022-11-25 23:00:03 +0400

    implement socket io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO= require('socket.io');
 
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
-var app = express();
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('connected');
+
+    io.on('disconnect', () => {
+        console.log('disconnected');
+    })
+})
+
+server.listen(port, () => {
     console.log(`server is running on ${port} port`);
 })
